4577159790b879ff76499a6466b4152785ba1d66
Raghu-KV <raghunandanv19@gmail.com>
2023-04-28 10:01:17 +0530

    bk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -14,13 +15,20 @@ console.log("mongodb connected");
 //_____________________________________________
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ message: "express working successfully" });
 });
 
-app.post("/", (req, res) => {
-  res.send({});
+app.get("/products", async (req, res) => {
+  const allProducts = await client
+    .db("pizza-delevery")
+    .collection("products")
+    .find({})
+    .toArray();
+
+  res.send(allProducts);
 });
 
 app.listen(PORT, () => console.log(`listening to PORT : ${PORT}`));
